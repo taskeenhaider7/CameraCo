8bed78a0399375ea7d0264ea16a356f1e51881da
Ishaq Shafiq <mishaq@stellatechnology.com>
2020-03-14 16:52:30 +0500

    pods installed successfully

--- android/app/src/main/java/com/camera/MainApplication.java
+++ android/app/src/main/java/com/camera/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 //import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
